6984cad89362ffc6095232c90daceb81a0b4636d
Selin <ozenselin@users.noreply.github.com>
2025-05-14 22:18:01 +0300

    install html-webpack-plugin and config html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -8,6 +9,11 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
     module: {
         rules: [
             {
