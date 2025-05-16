843c59c6f3593247d185d8902638bebc261fc823
Selin <ozenselin@users.noreply.github.com>
2025-05-16 23:55:31 +0300

    fix: clear contents of main in createHero function & add corresponding stylesheet reference

--- src/hero.js
+++ src/hero.js
@@ -1,10 +1,13 @@
+import './hero.css';
 export default loadHero;
 
 // text contents
 const HERO_HEADING = `Fresh and Healthy Food Specialties`;
 const HERO_SUBTEXT = `Variety of fresh food served just for you, your solution when hungry in the middle of the night with super fast delivery`;
 
+
 function createHero() {
+    //hero is a section element that has two children: heading & subtext
     const heroSubtext = document.createElement('p');
     heroSubtext.classList.add('hero__subtext');
     heroSubtext.textContent = HERO_SUBTEXT;
@@ -26,6 +29,8 @@ function loadHero() {
     //get main
     const main = document.querySelector('main');
 
+    //clear main
+    main.innerHTML = '';
     //create hero
     const hero = createHero();
 
