7f9a2ad3f3f074056a9ef820a7787ec862410ad1
Selin <ozenselin@users.noreply.github.com>
2025-05-16 23:50:56 +0300

    feat: initialise app & setup event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style.css';
+import loadHero from './hero.js';
+import loadAbout from './about.js';
+import loadMenu from './menu.js';
+
+//initialise app
+document.addEventListener('DOMContentLoaded', () => {
+    loadHero();
+});
+
+//add event listener to navlist
+//catch and handle click events using switch-id method for each item(button)
+const navList = document.querySelector('.nav-list');
+navList.addEventListener('click', (event) => {
+    const target = event.target;
+    switch(target.id){
+        case 'home-btn':
+            loadHero();
+            break;
+        case 'about-btn':
+            loadAbout();
+            break;
+        case 'menu-btn':
+            loadMenu();
+            break;
+        default:
+            break;
+    }
+});
